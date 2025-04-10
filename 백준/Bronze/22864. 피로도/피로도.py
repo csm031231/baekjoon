c1e16a714b1a3c56ba3bc4c822afb0303cbe8fc7
csm031231 <144320859+csm031231@users.noreply.github.com>
2025-04-10 20:57:57 +0900

    [Bronze II] Title: 피로도, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/22864. 피로도/피로도.py
+++ 백준/Bronze/22864. 피로도/피로도.py
@@ -4,10 +4,11 @@ day = 0        # 시간 (0시부터 시작해서 24시까지 체크)
 result = 0     # 일해서 번 돈 (또는 작업량)
 counts = 0     # 현재 피로도
 
-if a > m:
-    result = 0
+
+if a > m :
+    print(0)
 else:
-    while day != 24:
+    while day != 24 :
         day += 1
         if counts + a <= m:
             counts += a
@@ -17,5 +18,4 @@ else:
                 counts -= c
             else:
                 counts = 0  
-
-print(result)
+    print(result)
